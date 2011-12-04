d04c831c7e2dacd97e368511d4762807b37aa8d2
Paolo Dongilli <paolo.dongilli@gmail.com>
2011-12-04 20:57:23 +0100

    new Application subclass for storing application-wide variables

---
+++ src/it/sasabz/android/sasabus/SASAbus.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * SASAbus.java
+ * 
+ * Created: Dec 4, 2011 12:32:09 PM
+ * 
+ * Copyright (C) 2011 Paolo Dongilli
+ *
+ * This file is part of SASAbus.
+
+ * SASAbus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SASAbus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SASAbus.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package it.sasabz.android.sasabus;
+
+import android.app.Application;
+
+public class SASAbus extends Application {
+    private int dbDownloadAttempts;
+ 
+    @Override
+    public void onCreate() {
+        // Init values which could be loaded from files stored in res/raw
+        setDbDownloadAttempts(0);
+        super.onCreate();
+    }
+
+	/**
+	 * @param dbDownloadAttempts the dbDownloadAttempts to set
+	 */
+	public void setDbDownloadAttempts(int dbDownloadAttempts) {
+		this.dbDownloadAttempts = dbDownloadAttempts;
+	}
+
+	/**
+	 * @return the dbDownloadAttempts
+	 */
+	public int getDbDownloadAttempts() {
+		return dbDownloadAttempts;
+	}
+ 
+
+}
