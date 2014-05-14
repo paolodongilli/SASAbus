43d1599c8c172619aa6d244a429fb94cc9151ada
davide <d@vide.bz>
2014-05-14 19:28:20 +0200

    About in menu

--- src/it/sasabz/sasabus/ui/AboutFragment.java
+++ src/it/sasabz/sasabus/ui/AboutFragment.java
@@ -1,3 +1,27 @@
+/*
+ * SASAbus - Android app for SASA bus open data
+ *
+ * AboutFragment.java
+ *
+ * Created: May 14, 2014 19:24:00 PM
+ *
+ * Copyright (C) 2011-2014 Paolo Dongilli, Markus Windegger, Davide Montesin
+ *
+ * This file is part of SASAbus.
+ *
+ * SASAbus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SASAbus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SASAbus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package it.sasabz.sasabus.ui;
 
