d7cfbc763d7c988b90b891487c84cd0fcd59e0aa
davide <d@vide.bz>
2014-05-20 11:12:43 +0200

    parkings

---
+++ src/it/sasabz/sasabus/ui/ParkingData.java
@@ -0,0 +1,10 @@
+
+package it.sasabz.sasabus.ui;
+
+public class ParkingData
+{
+   String name;
+   int    free;
+   int    tot;
+
+}
--- src/it/sasabz/sasabus/ui/ParkingsFragment.java
+++ src/it/sasabz/sasabus/ui/ParkingsFragment.java
@@ -26,27 +26,94 @@
 package it.sasabz.sasabus.ui;
 
 import it.sasabz.android.sasabus.R;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class ParkingsFragment extends SherlockFragment
 {
-   LinearLayout ll;
 
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
    {
       final MainActivity mainActivity = (MainActivity) this.getActivity();
-      this.ll = new LinearLayout(mainActivity);
-      TextView searching = new TextView(mainActivity);
-      searching.setText(mainActivity.getString(R.string.searching_connection));
-      this.ll.addView(searching);
-      return this.ll;
+      LinearLayout ll = new LinearLayout(mainActivity);
+      ll.setBackgroundColor(Color.WHITE);
+
+      final ListView listView = new ListView(mainActivity);
+      final ArrayAdapter<String> adapter = new ArrayAdapter<String>(mainActivity,
+                                                                    android.R.layout.simple_list_item_1);
+
+      adapter.add(mainActivity.getString(R.string.searching_connection));
+      listView.setAdapter(adapter);
+      ll.addView(listView);
+
+      new Thread(new Runnable()
+      {
+         @Override
+         public void run()
+         {
+            try
+            {
+               String ids = IOUtils.toString(new URL("http://ipchannels.integreen-life.bz.it/parkingFrontEnd/rest/getParkingIds"));
+               JSONArray jsonArray = new JSONArray(ids);
+               int len = jsonArray.length();
+
+               final ParkingAdapter adapter = new ParkingAdapter(mainActivity);
+
+               adapter.clear();
+
+               for (int i = 0; i < len; i++)
+               {
+
+                  final ParkingData parkingData = new ParkingData();
+
+                  int pid = jsonArray.getInt(i);
+
+                  String infos = IOUtils.toString(new URL("http://ipchannels.integreen-life.bz.it/parkingFrontEnd/rest/getParkingStation?identifier="
+                                                          + pid));
+
+                  JSONObject jsonObject = new JSONObject(infos);
+
+                  parkingData.name = jsonObject.getString("name");
+                  //String parkingAddress = jsonObject.getString("address");
+                  parkingData.tot = Integer.parseInt(jsonObject.getString("slots"));
+
+                  parkingData.free = Integer.parseInt(IOUtils.toString(new URL("http://ipchannels.integreen-life.bz.it/parkingFrontEnd/rest/getNumberOfFreeSlots?identifier="
+                                                                               + pid)));
+
+                  adapter.add(parkingData);
+
+               }
+
+               mainActivity.runOnUiThread(new Runnable()
+               {
+                  @Override
+                  public void run()
+                  {
+                     listView.setAdapter(adapter);
+                  }
+               });
+            }
+            catch (Exception e)
+            {
+               mainActivity.handleApplicationException(e);
+            }
+
+         }
+      }).start();
+
+      return ll;
 
    }
 }
